5e7bed15073cbb596a62dcd2fc91f650009c159e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-15 19:47:19 +0800

    Revert "Updater: Improve update compatibility detection"
    This reverts commit 62898cbb189c3ae8ec89f74bb8c2620aad33b7e3.

--- app/src/main/java/org/lineageos/updater/misc/Utils.java
+++ app/src/main/java/org/lineageos/updater/misc/Utils.java
@@ -85,9 +85,17 @@ public class Utils {
     }
 
     public static boolean isCompatible(UpdateBaseInfo update) {
-        String currentVersion = SystemProperties.get(Constants.PROP_BUILD_VERSION);
-        if (update.getVersion().equalsIgnoreCase(currentVersion)) {
-            Log.d(TAG, update.getName() + " has the same firmware version");
+        if (update.getVersion().compareTo(SystemProperties.get(Constants.PROP_BUILD_VERSION)) < 0) {
+            Log.d(TAG, update.getName() + " is older than current Android version");
+            return false;
+        }
+        if (!SystemProperties.getBoolean(Constants.PROP_UPDATER_ALLOW_DOWNGRADING, false) &&
+                update.getTimestamp() <= SystemProperties.getLong(Constants.PROP_BUILD_DATE, 0)) {
+            Log.d(TAG, update.getName() + " is older than/equal to the current build");
+            return false;
+        }
+        if (!update.getType().equalsIgnoreCase(SystemProperties.get(Constants.PROP_RELEASE_TYPE))) {
+            Log.d(TAG, update.getName() + " has type " + update.getType());
             return false;
         }
         return true;
